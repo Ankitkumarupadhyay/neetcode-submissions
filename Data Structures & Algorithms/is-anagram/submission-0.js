fade181cb2a819a901e2307e3eec2b12526e5007
Ankit Kumar Upadhyay <159689608+Ankitkumarupadhyay@users.noreply.github.com>
2026-06-27 00:49:24 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.js
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -5,32 +5,22 @@ class Solution {
      * @return {boolean}
      */
     isAnagram(s, t) {
-        if(s.length != t.length){
+        if(s.length !== t.length){
             return false
         }
-        const arrayMap = new Map()
-        for(let i=0;i<s.length;i++){
-            if(arrayMap.has(s[i])){
-                arrayMap.set(s[i],arrayMap.get(s[i]) + 1)
-            }else{
-                arrayMap.set(s[i],1)
-            }
-        }
 
-        for(let i=0;i<t.length;i++){
-            if(arrayMap.has(t[i])){
-                const value = arrayMap.get(t[i])
-                if(value >1){
-arrayMap.set(t[i],(value - 1))
-                }else{
-                    arrayMap.delete(t[i])
-                }
-                
-            }else{
-                return false
-            }
-        }
+        const count = {};
 
-        return arrayMap.size == 0 ? true : false
+    for (const char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (const char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+
+    return true;
+        
     }
 }
